06592e8ecf6b1b086793090d340e9c8abb74ccbb
Christian Carmona <carmona@stats.ox.ac.uk>
2020-02-20 22:55:57 +0000

    biased data example

---
+++ R/biased_data.R
@@ -0,0 +1,133 @@
+
+#' @export
+mu_b_bt_post = function( Z, Y, sigma_z, sigma_y, gamma, rho, rho_tilde, eta=1 ){
+  
+  # Calculate de posterior mean and variance of the Semi-Modular Posterior
+  # Z, Y : data from the unbiased and biased modules, respectively
+  # eta : degree of influence of the unbiased module into mu
+  # sigma_z, sigma_y : likelihood std for Z and Y, respectively
+  # gamma, rho : prior std for mu and b, respectively
+  
+  # browser()
+  
+  n = length(Z)
+  m = length(Y)
+  
+  # if eta==0:
+  # eta = 0.0000001
+  # rho_tilde = rho * np.sqrt(1/eta)
+  # rho_tilde = rho * 1
+  
+  A = matrix(0,3,3)
+  A[1,1] = (n/(sigma_z^2)) + (m/(sigma_y^2)) * (1+eta) + 1 / gamma^2 - (m/(sigma_y^2+m*rho^2))
+  A[2,2] = m/(sigma_y^2) + 1/(rho^2)
+  A[3,3] = eta * m/(sigma_y^2) + 1/(rho_tilde^2)
+  A[1,2] = A[2,1] = m/(sigma_y^2)
+  A[1,3] = A[3,1] = eta*m/(sigma_y^2)
+  
+  cov = solve(A)
+  
+  aux = matrix(0,3,1)
+  aux[1,1] = sum(Z)/sigma_z^2 + (sum(Y)/sigma_y^2) * ( (eta*(sigma_y^2 + m*rho^2) + m*rho^2)/(sigma_y^2 + m*rho^2))
+  aux[2,1] = sum(Y)/sigma_y^2
+  aux[3,1] = eta*sum(Y)/sigma_y^2
+  mean = cov %*% aux
+  
+  return( list(mean, cov) )
+}
+
+#' @export
+mu_b_post = function( Z, Y, sigma_z, sigma_y, gamma, rho, eta=1 ){
+  # Calculate de posterior mean and variance of the Semi-Modular Posterior
+  # Z, Y : data from the unbiased and biased modules, respectively
+  # eta : degree of influence of the unbiased module into mu
+  # sigma_z, sigma_y : likelihood std for Z and Y, respectively
+  # gamma, rho : prior std for mu and b, respectively
+  n = length(Z)
+  m = length(Y)
+  
+  A = matrix(0,2,2)
+  A[1,1] = (n/(sigma_z^2)) + (m/(sigma_y^2)) * (( eta*sigma_y^2 + m * rho^2)/( sigma_y^2 + m * rho^2)) + 1 / gamma^2
+  A[1,2] = A[1,0] = m/(sigma_y^2)
+  A[2,2] = m/(sigma_y^2) + 1/(rho^2)
+  cov = solve(A)
+  
+  aux = matrix(0,2,1)
+  aux[1,1] = sum(Z)/sigma_z^2 + (sum(Y)/sigma_y^2) * ((eta*sigma_y^2 + m * rho^2)/(sigma_y^2 + m * rho^2))
+  aux[2,1] = sum(Y)/sigma_y^2
+  mean = cov %*% aux
+  return( list(mean, cov) )
+}
+
+#' @export
+mu_post = function( Z, Y, sigma_z, sigma_y, gamma, rho, eta=1 ) {
+  # Calculate de posterior mean and variance of the Semi-Modular Posterior
+  # Z, Y : data from the unbiased and biased modules, respectively
+  # eta : degree of influence of the unbiased module into mu
+  # sigma_z, sigma_y : likelihood std for Z and Y, respectively
+  # gamma, rho : prior std for mu and b, respectively
+  
+  n = length(Z)
+  m = length(Y)
+  
+  var = n / sigma_z^2 + eta * ( m /( sigma_y^2 + m * rho^2) ) + 1 / gamma^2
+  var = 1/var
+  
+  mean = sum(Z)/sigma_z^2 + eta * sum(Y) / (sigma_y^2 + m * rho^2)
+  mean = mean * var
+  
+  return( list(mean, var) )
+  
+}
+
+#' @export
+predict_post = function( Z, Y, sigma_z, sigma_y, gamma, rho, eta=1, stage2=True ) {
+  
+  # Calculate de Predictive posterior mean and variance of the Semi-Modular Posterior
+  
+  # Z, Y : data from the unbiased and biased modules, respectively
+  # eta : degree of influence of the unbiased module into mu
+  # sigma_z, sigma_y : likelihood std for Z and Y, respectively
+  # gamma, rho : prior std for mu and b, respectively
+  
+  
+  n = length(Z)
+  m = length(Y)
+  
+  # Posterior of mu, b, b_tilde
+  aux = mu_b_bt_post( Z, Y, sigma_z, sigma_y, gamma, rho, rho, eta )
+  mean_post = aux[[1]]
+  cov_post = aux[[2]]
+  rm(aux)
+  
+  # mean_post, cov_post = mu_b_post( Z, Y, sigma_z, sigma_y, gamma, rho, eta )
+  
+  if (stage2){ # keep b
+    mean_post = mean_post[c(1,2)]
+    cov_post = cov_post[c(1,2),c(1,2)]
+  } else{ # keep b tilde
+    mean_post = mean_post[c(1,3)]
+    cov_post = cov_post[c(1,3),c(1,3)]
+  }
+  
+  cov_post_inv = solve(cov_post)
+  
+  A = matrix(0,4,4)
+  
+  A[1,1] = 1/(sigma_z^2)
+  A[2,2] = 1/(sigma_y^2)
+  A[3,3] = 1/(sigma_z^2)
+  A[2:4,2:4] = A[2:4,2:4] + 1/(sigma_y^2)
+  A[2:4,2:4] = A[2:4,2:4] + cov_post_inv
+  
+  A[1,3] = A[3,1] = -1/(sigma_z^2)
+  A[2,3] = A[3,2] = -1/(sigma_y^2)
+  A[2,4] = A[4,2] = -1/(sigma_y^2)
+  
+  cov = solve(A)
+  aux = matrix(0,4,1)
+  aux[2:4,] = cov_post_inv %*% mean_post
+  mean = cov %*% aux
+  
+  return( list( mean[c(0,1),], cov[c(0,1),c(0,1)] ) )
+}
---
+++ inst/sec_5_1.R
@@ -0,0 +1,172 @@
+rm(list = ls())
+options(scipen=999, stringsAsFactors=FALSE)
+set.seed(0)
+
+# pkgbuild::compile_dll()
+# Rcpp::compileAttributes()
+# devtools::document()
+# devtools::install()
+
+# loading required packages #
+req.pck <- c( "aistats2020smi",
+              "tidyverse","foreach","doParallel","doRNG","cowplot",
+              "abind" )
+req.pck_bool <- sapply(X=req.pck,FUN=require,character.only=T)
+if(!all(req.pck_bool)) {
+  sapply(X=req.pck[!req.pck_bool],FUN=install.packages,character.only=T);
+  sapply(X=req.pck,FUN=require,character.only=T)
+}
+
+# Parallel processing
+parallel_comp = TRUE
+if(parallel_comp){
+  n_cores = 6
+  options(cores=n_cores)
+  doParallel::registerDoParallel()
+  getDoParWorkers()
+}
+
+### Generative (true) parameters ###
+
+mu = 0
+b = 1
+n=25 # Sample size for Z
+m=50 # Sample size for Y
+sigma_z = 2 # Likelihood variance for Z
+sigma_y = 1 # Likelihood variance for Y
+gamma=Inf # Prior variance mu
+rho=0.5 # Prior variance eta
+
+param_names = c('mu','b','b_tilde')
+param_true = c(mu,b,b)
+
+# sequence of eta values in (0,1)
+eta_all = seq(0,1,0.01)
+
+
+# Illustrate convenience of SMI with one synthetic dataset #
+if(F) {
+  set.seed(123)
+  Z = rnorm( n=n, mean=mu, sd=sigma_z)
+  Y = rnorm( n=m, mean=mu+b, sd=sigma_y )
+  cat('Z_mean=',mean(Z),'; Y_mean=',mean(Y))
+  
+  comb <- function(...) { mapply('rbind', ..., SIMPLIFY=FALSE) }
+  # acomb <- function(...) { abind(..., along=3) }
+  post_eta_all = foreach::foreach(eta = eta_all,.combine='comb', .multicombine=TRUE) %do% {
+    # eta = 0.01
+    posterior = aistats2020smi::mu_b_bt_post( Z=Z, Y=Y, sigma_z=sigma_z, sigma_y=sigma_y, gamma=gamma, rho=rho, rho_tilde=rho, eta=eta )
+    list( t(posterior[[1]]), diag(posterior[[2]]) )
+  }
+  
+  aistats2020smi::set_ggtheme()
+  
+  # Posterior vs True value
+  post_plot_all = foreach::foreach( par_i = seq_along(param_names) ) %do% {
+    
+    p = data.frame( eta=eta_all,
+                    post_mean=post_eta_all[[1]][,par_i],
+                    post_sd=post_eta_all[[2]][,par_i]^0.5 ) %>%
+      ggplot() + 
+      geom_line( aes(x=eta,y=post_mean), col='red' ) +
+      geom_line( aes(x=eta,y=post_mean+post_sd), col='blue', lty=3 ) +
+      geom_line( aes(x=eta,y=post_mean-post_sd), col='blue', lty=3 ) +
+      geom_hline(yintercept=param_true[par_i], lty=2) +
+      labs(y=paste(param_names[par_i]," posterior",sep=""))
+    p
+  }
+  cowplot::plot_grid(  post_plot_all[[1]], post_plot_all[[2]], post_plot_all[[3]], ncol=1, align='v' )
+  
+  # MSE
+  mse_plot_all = foreach::foreach( par_i = seq_along(param_names) ) %do% {
+    # par_i=3
+    p = data.frame( eta=eta_all,
+                    mse=post_eta_all[[2]][,par_i] + (post_eta_all[[1]][,par_i]-param_true[par_i])^2 ) %>%
+      ggplot() + 
+      geom_line( aes(x=eta,y=mse), col='red' ) +
+      labs(y=paste(param_names[par_i]," MSE",sep=""))
+    p
+  }
+  cowplot::plot_grid(  mse_plot_all[[1]], mse_plot_all[[2]], mse_plot_all[[3]], ncol=1, align='v' )
+}
+
+# Illustrate convenience of SMI in expectation #
+if(T) {
+  set.seed(1)
+  
+  # Average Mean Square Error #
+  
+  # Compute Posterior mean and sd for each iteration
+  n_iter = 1000
+  comb <- function(...) { mapply('rbind', ..., SIMPLIFY=FALSE) }
+  acomb <- function(...) { mapply('abind', ..., MoreArgs=list(along=3),SIMPLIFY=FALSE) }
+  post_eta_all_iter = foreach::foreach(iter_i = 1:n_iter,.combine='acomb', .multicombine=TRUE)  %dorng% {
+    # iter_i=1
+    Z = rnorm( n=n, mean=mu, sd=sigma_z)
+    Y = rnorm( n=m, mean=mu+b, sd=sigma_y )
+    post_eta_all = foreach::foreach(eta_i = seq_along(eta_all),.combine='comb', .multicombine=TRUE) %do% {
+      # eta_i=1
+      posterior = aistats2020smi::mu_b_bt_post( Z=Z, Y=Y, sigma_z=sigma_z, sigma_y=sigma_y, gamma=gamma, rho=rho, rho_tilde=rho, eta=eta_all[eta_i] )
+      list( t(posterior[[1]]), diag(posterior[[2]]) )
+    }
+    # post_eta_all[[1]]
+    post_eta_all
+  }
+  
+  # Compute MSE for each iteration
+  param_true_array = aperm( array(param_true,dim=c(3,length(eta_all),n_iter)) , c(2,1,3) )
+  MSE_all_iter = post_eta_all_iter[[2]] + (post_eta_all_iter[[2]]-param_true_array)^2
+  
+  # eta_i=1; iter_i=250
+  # MSE_all_iter[eta_i,,iter_i]
+  # post_eta_all_iter[[2]][eta_i,,iter_i] + (post_eta_all_iter[[2]][eta_i,,iter_i]-param_true)^2
+  
+  # Average across iterations
+  post_eta_all_average = list( apply(post_eta_all_iter[[1]],c(1,2),mean),
+                               apply(post_eta_all_iter[[2]],c(1,2),mean) + apply(post_eta_all_iter[[1]],c(1,2),var))
+  MSE_average = apply(MSE_all_iter,c(1,2),mean)
+  
+  
+  # Posterior vs True value
+  post_plot_all = foreach::foreach( par_i = seq_along(param_names) ) %do% {
+    
+    p = data.frame( eta=eta_all,
+                    post_mean=post_eta_all_average[[1]][,par_i],
+                    post_sd=post_eta_all_average[[2]][,par_i]^0.5 ) %>%
+      ggplot() + 
+      geom_line( aes(x=eta,y=post_mean), col='red' ) +
+      geom_line( aes(x=eta,y=post_mean+post_sd), col='blue', lty=3 ) +
+      geom_line( aes(x=eta,y=post_mean-post_sd), col='blue', lty=3 ) +
+      geom_hline(yintercept=param_true[par_i], lty=2) +
+      labs(y=paste(param_names[par_i]," posterior",sep=""))
+    p
+  }
+  p = cowplot::plot_grid(  post_plot_all[[1]], post_plot_all[[2]], post_plot_all[[3]], ncol=1, align='v' )
+  print(p)
+  
+  # MSE
+  aistats2020smi::set_ggtheme()
+  mse_plot_all = foreach::foreach( par_i = seq_along(param_names) ) %do% {
+    # par_i=3
+    p = data.frame( eta=eta_all,
+                    mse=MSE_average[,par_i] ) %>%
+      ggplot() + 
+      geom_line( aes(x=eta,y=mse), col='red' ) +
+      labs(y=paste(param_names[par_i]," MSE",sep=""))
+    p
+  }
+  cowplot::plot_grid(  mse_plot_all[[1]], mse_plot_all[[2]], mse_plot_all[[3]], ncol=1, align='v' )
+  
+  # b vs b_tilde
+  aistats2020smi::set_ggtheme()
+  p = MSE_average %>%
+    `colnames<-`(param_names) %>%
+    as.data.frame() %>%
+    mutate(eta=eta_all) %>%
+    tidyr::pivot_longer(cols=param_names,names_to='parameter') %>%
+    dplyr::filter(parameter %in% c('b','b_tilde')) %>%
+    ggplot() +
+    geom_line( aes(x=eta,y=value,col=parameter) ) +
+    labs(y="MSE average")
+  p
+}
